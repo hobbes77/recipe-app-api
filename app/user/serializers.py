301dceebd57b37d5eb5d7c06a5292b4d337b48e5
hobbes77 <jmatis@gmail.com>
2022-09-26 10:29:03 -0400

    capitals...

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -27,7 +27,7 @@ class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
     email = serializers.EmailField()
     password = serializers.CharField(
-        Style={'input_type': 'password'},
+        style={'input_type': 'password'},
         trim_whitespace=False,
     )
 
