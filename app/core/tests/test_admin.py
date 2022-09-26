449adcfeb367a92f4f14b100916411951b5dd43d
hobbes77 <jmatis@gmail.com>
2022-09-26 08:11:58 -0400

    various fixes

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,10 +1,10 @@
 """
-Tests for the Django admin modifications."""
+Tests for the Django admin modifications.
+"""
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
-
 
 
 class AdminSiteTests(TestCase):
