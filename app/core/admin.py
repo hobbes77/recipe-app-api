d5e64462aa403b6853fb8f54dbfe90db48477eb9
hobbes77 <jmatis@gmail.com>
2022-09-26 06:33:11 -0400

    dumb login typo

--- app/core/admin.py
+++ app/core/admin.py
@@ -26,7 +26,7 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
-    readonly_fields = ['last_ligin']
+    readonly_fields = ['last_login']
 
-    
+
 admin.site.register(models.User, UserAdmin)
